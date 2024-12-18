acaa3a02fee77ea1f0222676ce5869521f658bd9
Steven Lawson <steven.lawson@healthsqyre.com>
2024-12-18 10:43:20 -0700

    adding pagination

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.urls import path
-from .views import users, register, login, AuthenticatedUser, logout
+from .views import (
+	register, login, AuthenticatedUser, PermissionAPIView, logout, RoleViewSet, UserGenericAPIView
+)
 
 urlpatterns = [
-    path('users/', users),
     path('register/', register),
 	path('login/', login),
 	path('logout/', logout),
-	path('user/', AuthenticatedUser.as_view())
-
+	path('user/', AuthenticatedUser.as_view()),
+	path('permissions/', PermissionAPIView.as_view()),
+	path('roles/', RoleViewSet.as_view({
+		'get': 'list',
+		'post': 'create',
+	})),
+	path('roles/<str:pk>', RoleViewSet.as_view({
+		'get': 'retrieve',
+		'put': 'update',
+		'delete': 'destroy'
+	})),
+	path('users/', UserGenericAPIView.as_view()),
+	path('users/<str:pk>', UserGenericAPIView.as_view())
 ]
